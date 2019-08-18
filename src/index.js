1bc9838b5c1d495a97158e04b5bf09441ffd776e
Johan <jeus1609@gmail.com>
2019-08-17 22:14:41 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+//comentario linea
+
+
+// mas comentario linea
+
+//Otro comentario mas  y no se porque se resalta esa linea amarilla que ya me esta jodiendo y parece que es el git
