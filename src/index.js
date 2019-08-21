8f325c9bbbcc7837e762104c1601264ff898fda0
TIC_Chincheros <jeus1609>
2019-08-21 17:18:29 -0500

    insertar funcion ganador

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class Board extends React.Component {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
-            xIsnext: !this.state.xIsNext,
+            xIsNext: !this.state.xIsNext,
         });
     }
 
@@ -77,6 +77,27 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calcularGanador(squares) {
+    const lineas = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lineas.length; i++) {
+        const [a, b, c] = lineas[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 // ========================================
